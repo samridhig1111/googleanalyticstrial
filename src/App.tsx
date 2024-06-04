a4d5e21ddb4eda02439e9630de0ef1828451435c
Samridhi  Gupta <samridhig1111@gmail.com>
2024-06-04 17:17:30 +0530

    UPDATED MEASUREMENT ID

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import REACTGA from "react-ga";
 import React from "react";
 
-const TRACKING_ID = "G-87MR5FL9RG";
+const TRACKING_ID = "G-WCC55R5EL0";
 REACTGA.initialize(TRACKING_ID)
 
 function App() {
